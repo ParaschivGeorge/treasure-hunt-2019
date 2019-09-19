8e38693f75b379c918bbb8abbae0fc12c9d405f5
ParaschivGeorge <georgegmtt@gmail.com>
2019-09-19 19:29:35 +0300

    register forms

--- src/app/components/register-solo/register-solo.component.ts
+++ src/app/components/register-solo/register-solo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Participant} from '../../models/participant';
 
 @Component({
   selector: 'app-register-solo',
@@ -7,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterSoloComponent implements OnInit {
 
+  registerSoloForm: FormGroup = new FormGroup({
+    lastName: new FormControl(null, [Validators.required]),
+    firstName: new FormControl(null, [Validators.required]),
+    phone: new FormControl(null, [Validators.required]),
+    university: new FormControl({ value: 'Automatica si Calculatoare', disabled: true }, [Validators.required]),
+    facebookLink: new FormControl(null, ),
+    team: new FormControl({ value: null, disabled: true }),
+    preferredTeammate: new FormControl(null)
+  });
+
   constructor() { }
 
   ngOnInit() {
     window.scrollTo({top: 0, behavior: 'auto'});
   }
 
+  registerSolo() {
+    if (this.registerSoloForm.valid) {
+      console.log(this.registerSoloForm.getRawValue() as Participant);
+    }
+  }
 }
--- src/app/components/register-team/register-team.component.ts
+++ src/app/components/register-team/register-team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Participant} from '../../models/participant';
 
 @Component({
   selector: 'app-register-team',
@@ -9,8 +11,62 @@ export class RegisterTeamComponent implements OnInit {
 
   constructor() { }
 
+  teamNameFormControl: FormControl = new FormControl(null, Validators.required);
+
+  registerTeam1Form: FormGroup = new FormGroup({
+    lastName: new FormControl(null, [Validators.required]),
+    firstName: new FormControl(null, [Validators.required]),
+    phone: new FormControl(null, [Validators.required]),
+    university: new FormControl({ value: 'Automatica si Calculatoare', disabled: true }, [Validators.required]),
+    facebookLink: new FormControl(null, ),
+    team: new FormControl(null),
+    preferredTeammate: new FormControl({value: null, disabled: true})
+  });
+
+  registerTeam2Form: FormGroup = new FormGroup({
+    lastName: new FormControl(null, [Validators.required]),
+    firstName: new FormControl(null, [Validators.required]),
+    phone: new FormControl(null, [Validators.required]),
+    university: new FormControl({ value: 'Automatica si Calculatoare', disabled: true }, [Validators.required]),
+    facebookLink: new FormControl(null, ),
+    team: new FormControl(null),
+    preferredTeammate: new FormControl({value: null, disabled: true})
+  });
+
+  registerTeam3Form: FormGroup = new FormGroup({
+    lastName: new FormControl(null, [Validators.required]),
+    firstName: new FormControl(null, [Validators.required]),
+    phone: new FormControl(null, [Validators.required]),
+    university: new FormControl('Automatica si Calculatoare', [Validators.required]),
+    facebookLink: new FormControl(null, ),
+    team: new FormControl(null),
+    preferredTeammate: new FormControl({value: null, disabled: true})
+  });
+
+  registerTeamForm: FormGroup = new FormGroup({
+    teamName: this.teamNameFormControl,
+    member1: this.registerTeam1Form,
+    member2: this.registerTeam2Form,
+    member3: this.registerTeam3Form,
+  });
+
   ngOnInit() {
     window.scrollTo({top: 0, behavior: 'auto'});
+    console.log(this.registerTeamForm);
+  }
+
+  registerTeam() {
+    if (this.registerTeamForm.valid) {
+      const formValue = this.registerTeamForm.getRawValue();
+      const teamName: string = formValue.teamName;
+      const participant1: Participant = formValue.member1 as Participant;
+      const participant2: Participant = formValue.member2 as Participant;
+      const participant3: Participant = formValue.member3 as Participant;
+      participant1.team = teamName;
+      participant2.team = teamName;
+      participant3.team = teamName;
+      console.log(teamName, participant1, participant2, participant3);
+    }
   }
 
 }
