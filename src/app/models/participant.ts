0cec99bde9ea92037f623cc5c08c1b8be07d4376
ParaschivGeorge <georgegmtt@gmail.com>
2019-09-19 19:33:36 +0300

    register forms

---
+++ src/app/models/participant.ts
@@ -0,0 +1,9 @@
+export interface Participant {
+  lastName: string;
+  firstName: string;
+  phone: string;
+  university: string;
+  facebookLink: string;
+  team: string;
+  preferredTeammate: string;
+}
